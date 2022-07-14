092c947445b48c844547fb430dad0b9219c3c3d5
Jean Paulo <jean.paulo@edu.itajai.sc.gov.br>
2022-07-14 16:01:24 -0300

    project

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import Backdrop from '@mui/material/Backdrop';
+import CircularProgress from '@mui/material/CircularProgress';
+import Button from '@mui/material/Button';
+
+export const Header = () => {
+
+ return(
+<header>
+  <h1>Mundo</h1>
+  <hr/>
+</header>
+
+ );
+
+
+}
